47c94f52820c825fde9533586548992deae6d94f
Abhishek Wadhwa <99806093+001abhishekk@users.noreply.github.com>
2025-11-02 14:51:59 +0530

    Update Google Maps embed link in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
         <div className="grid lg:grid-cols-2 gap-12">
           <div className="relative h-[400px] lg:h-[500px] rounded-2xl overflow-hidden shadow-2xl">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3468.123456789!2d76.828!3d29.963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjnCsDU3JzQ2LjgiTiA3NsKwNDknNDAuOCJF!5e0!3m2!1sen!2sin!4v1234567890"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.2854281122864!2d76.8422241!3d29.9712261!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390e47443a2fb66b%3A0x180654f25acd73c7!2sSharma%20Bakers!5e0!3m2!1sen!2sin!4v1762074849302!5m2!1sen!2sin"
               width="100%"
               height="100%"
               style={{ border: 0 }}
